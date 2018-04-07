6074cd40f2ea7e57e05ee1891ef818018c168aa4
maximus <maxcamer@gmail.com>
2018-04-07 18:25:50 +0300

    070418

---
+++ Main#4/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Main4
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			int num, num_1;
+	
+			Console.WriteLine ("Enter first num: ");
+			num = Convert.ToInt32 (Console.ReadLine ());
+			Console.WriteLine ("\nEnter second num: ");
+			num_1 = Convert.ToInt32 (Console.ReadLine ());
+			Console.WriteLine ("The result is: " + (num + num_1). ToString ());
+			Console.ReadKey ();
+		}
+	}
+}
--- README.md
+++ README.md
@@ -1,2 +1,1 @@
 # Main-4
-my desc
